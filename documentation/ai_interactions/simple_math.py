3700f9cb05144a7c96db72974d274fbde4fcfb7e
Sierra Martinez-Kratz <sierramartinez-kratz@Marinas-MacBook-Air-54.local>
2025-10-23 12:20:05 -0400

    Add add_numbers function and move factorial function

---
+++ documentation/ai_interactions/simple_math.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    """
+    Adds two numbers together.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
